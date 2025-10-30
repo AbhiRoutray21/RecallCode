3c97731c4eb9158464dfaabe68178d8231ed84df
Abhijit <routrayabhijit4@gmail.com>
2025-10-30 18:07:38 +0530

    add cookie consent popup.

--- Client/src/Layouts/MainLayout.jsx
+++ Client/src/Layouts/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation,NavLink } from "react-router-dom";
 import { lazy, Suspense, useEffect, useState } from "react";
 import Sidebar from "../components/sidebar/sidebar";
 import LoginPopMessage from "../components/loginMessage/loginPop";
@@ -8,15 +8,28 @@ import useAuth from "../hooks/useAuth";
 const SessionExpire = lazy(() => import('../components/sessionExpire/expire'));
 const SettingsPop = lazy(() => import('../components/settings/settings'));
 
+/*helper*/
+function getItemFromStorage(name) {
+  const value = localStorage.getItem(name);
+  if (value === null) return false;
+  return value === 'true'; // will return true or false
+}
+
 export default function MainLayout() {
   const {expire} = useAuth();
   const [settingsPop, setSettingsPop] = useState(false);
   const {auth} = useAuth();
   const location = useLocation();
+  const [consent, setConsent] = useState(() => getItemFromStorage('cookieConsent'));
 
   useEffect(() => {
       setSettingsPop(location.hash.startsWith("#settings"));
   }, [location.hash]);
+
+  function comfirm(){
+    setConsent(true);
+    localStorage.setItem('cookieConsent',true);
+  }
 
   return (
     <MainLayoutProvider>
@@ -29,7 +42,19 @@ export default function MainLayout() {
           <Outlet />
         </div>
 
+        {/* login popup box */}
         <LoginPopMessage />
+
+        {/* cookie consent popup */}
+        {auth?.accessToken && !consent &&
+          <div className="privacy-notice-pop">
+            <p>
+              By continuing to use this website, you agree to our use of cookies.
+              You can read our
+              <NavLink to={'http://localhost:5174/privacy-policy'} target="_blank" rel="noopener noreferrer" className={'notice-link'}>Privacy Policy</NavLink> and <NavLink to={'http://localhost:5174/terms'} target="_blank" rel="noopener noreferrer" className={'notice-link'}>Terms & Conditions</NavLink> for more information.</p>
+            <div onClick={comfirm}>Got it</div>
+          </div>
+        }
 
         {/* session expire popup */}
         {expire && (
@@ -44,6 +69,7 @@ export default function MainLayout() {
             <SettingsPop />
           </Suspense>
         )}
+
       </div>
     </MainLayoutProvider>
   );
