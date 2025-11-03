7e0fe4bf7439e1b80a8eea566676a620cbd7be46
Abhijit <routrayabhijit4@gmail.com>
2025-11-03 19:08:46 +0530

    minor change

--- Server/model/PracticeSession.js
+++ Server/model/PracticeSession.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const PracticeSessionSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true},
   language: { type: String, required: true },
   rounds: [
     {
