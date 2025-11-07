54265669773380cdff892220b27d69dfc2c74c9e
Abhijit <routrayabhijit4@gmail.com>
2025-11-07 14:07:33 +0530

    remove the tryCatch block that i previously added. because the error from this api call is handle inside useAxiosPrivate hook.  and it is nessacery for session expire ditection and popup.

--- Client/src/hooks/useRefreshToken.js
+++ Client/src/hooks/useRefreshToken.js
@@ -5,22 +5,22 @@ const useRefreshToken = () => {
   const { setAuth } = useAuth();
 
     const refresh = async () => {
-      try {
-        const response = await axiosBase.get("/refresh");
-        setAuth(prev => {
-            return { 
-              ...prev,
-              name: response.data.name,
-              accessToken: response.data.accessToken 
-            }
-        }); 
-        return response.data.accessToken;
-      } catch (error) {
-        
-      }
+      const response = await axiosBase.get("/refresh");
+      setAuth(prev => {
+          return { 
+            ...prev,
+            name: response.data.name,
+            accessToken: response.data.accessToken 
+          }
+      }); 
+      return response.data.accessToken;
     }
 
   return refresh;
 };
+
+/* don't wrap this code into tryCatch block, 
+because the error from this api call is handle inside useAxiosPrivate hook.  
+and it is nessacery for session expire ditection and popup*/
 
 export default useRefreshToken;
